f627e54c2c4fe20d2f264cd9f0394cc360af530c
mokshagnachintha <c.mokshagna@gmail.com>
2025-12-19 17:46:39 +0530

    thrid commit

---
+++ chatmodels/huggingface.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    max_new_tokens=100,
+    temperature=0.5
+)
+
+print(llm.invoke("What is the capital of India?"))
